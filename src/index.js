2fa360ecc444c4df752fb0a2473ed1630c361d90
andr3w <liu.anray+github@gmail.com>
2018-06-22 22:26:00 -0400

    Convert from callbacks to promises

--- src/index.js
+++ src/index.js
@@ -1,39 +1,29 @@
 /* eslint-disable no-console */
 import { MongoClient } from 'mongodb';
 
-const insertDocuments = (collection, callback) => {
-  collection.insertMany([
-    { a: 1 }, { a: 2 }, { a: 3 },
-  ], (err, result) => {
-    console.log('Inserted 3 documents into the collection.');
-    callback(result);
-  });
-};
+const insertDocuments = collection => collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+const findDocuments = collection => collection.find({}).toArray();
 
-const findDocuments = (collection, callback) => {
-  collection.find({}).toArray((err, docs) => {
-    console.log('Found the following records:');
-    console.log(docs);
-    callback(docs);
-  });
-};
-
-const uri = 'mongodb://kotetsu:qv4zvvcbfV3XeAxw@cluster0-shard-00-00-spwtd.mongodb.net:27017,'
+const URI = 'mongodb://kotetsu:qv4zvvcbfV3XeAxw@cluster0-shard-00-00-spwtd.mongodb.net:27017,'
   + 'cluster0-shard-00-01-spwtd.mongodb.net:27017,cluster0-shard-00-02-spwtd.mongodb.net:27017/test?'
   + 'ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
-MongoClient.connect(uri, (err, client) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+
+MongoClient.connect(URI).then((client) => {
   const db = client.db('wildTigerDb');
   console.info('Connected to database.');
   const collection = db.collection('wildTigerCollection');
-  insertDocuments(collection, () => {
-    findDocuments(collection, () => {
+  insertDocuments(collection).then(() => {
+    console.info('Inserted documents into the collection.');
+    findDocuments(collection).then((docs) => {
+      console.info('Found the following records:');
+      console.info(docs);
       collection.drop().then(() => {
-        client.close();
+        console.info('Dropped collection.');
+        const force = false;
+        client.close(force).then(() => {
+          console.info('Closed connection to database.');
+        }).catch(error => console.error(error));
       });
-    });
-  });
-});
+    }).catch(error => console.error(error));
+  }).catch(error => console.error(error));
+}).catch(error => console.error(error));
