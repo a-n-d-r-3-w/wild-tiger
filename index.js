cc63e4170fb67a947891f871827c02d2e57565b3
andr3w <liu.anray+github@gmail.com>
2018-06-22 21:51:42 -0400

    Install and configure ESLint

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
-var MongoClient = require('mongodb').MongoClient;
+/* eslint-disable no-console */
+const { MongoClient } = require('mongodb');
 
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
+const insertDocuments = (db, callback) => {
+  const collection = db.collection('documents');
   collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    // assert.equal(err, null);
-    // assert.equal(3, result.result.n);
-    // assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
+    { a: 1 }, { a: 2 }, { a: 3 },
+  ], (err, result) => {
+    console.log('Inserted 3 documents into the collection');
     callback(result);
   });
-}
+};
 
-var findDocuments = function(db, callback) {
+const findDocuments = (db, callback) => {
   // Get the documents collection
-  var collection = db.collection('documents');
+  const collection = db.collection('documents');
   // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    // assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs)
+  collection.find({}).toArray((err, docs) => {
+    console.log('Found the following records');
+    console.log(docs);
     callback(docs);
   });
-}
+};
 
-var uri = "mongodb://kotetsu:qv4zvvcbfV3XeAxw@cluster0-shard-00-00-spwtd.mongodb.net:27017,cluster0-shard-00-01-spwtd.mongodb.net:27017,cluster0-shard-00-02-spwtd.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
-MongoClient.connect(uri, function(err, client) {
+const uri = 'mongodb://kotetsu:qv4zvvcbfV3XeAxw@cluster0-shard-00-00-spwtd.mongodb.net:27017,cluster0-shard-00-01-spwtd.mongodb.net:27017,cluster0-shard-00-02-spwtd.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+MongoClient.connect(uri, (err, client) => {
   if (err) {
     console.error(err);
     return;
   }
-  let db = client.db('test');
+  const db = client.db('test');
   console.info('Connected to database with no error.');
-  insertDocuments(db, function() {
-    findDocuments(db, function() {
-      var collection = db.collection('documents');
+  insertDocuments(db, () => {
+    findDocuments(db, () => {
+      const collection = db.collection('documents');
       collection.drop().then(() => {
         client.close();
-      })
+      });
     });
   });
 });
