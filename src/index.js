4e513ed242705d41f0e75c9f9fadf3369de0de28
andr3w <liu.anray+github@gmail.com>
2018-06-22 22:34:06 -0400

    Convert chained .then()s into awaits and install Babel polyfill

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+import 'babel-polyfill';
 import { MongoClient } from 'mongodb';
 
 const insertDocuments = collection => collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
@@ -8,22 +9,18 @@ const URI = 'mongodb://kotetsu:qv4zvvcbfV3XeAxw@cluster0-shard-00-00-spwtd.mongo
   + 'cluster0-shard-00-01-spwtd.mongodb.net:27017,cluster0-shard-00-02-spwtd.mongodb.net:27017/test?'
   + 'ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 
-MongoClient.connect(URI).then((client) => {
+MongoClient.connect(URI).then(async (client) => {
   const db = client.db('wildTigerDb');
   console.info('Connected to database.');
   const collection = db.collection('wildTigerCollection');
-  insertDocuments(collection).then(() => {
-    console.info('Inserted documents into the collection.');
-    findDocuments(collection).then((docs) => {
-      console.info('Found the following records:');
-      console.info(docs);
-      collection.drop().then(() => {
-        console.info('Dropped collection.');
-        const force = false;
-        client.close(force).then(() => {
-          console.info('Closed connection to database.');
-        }).catch(error => console.error(error));
-      });
-    }).catch(error => console.error(error));
-  }).catch(error => console.error(error));
+  await insertDocuments(collection);
+  console.info('Inserted documents into the collection.');
+  const docs = await findDocuments(collection);
+  console.info('Found the following records:');
+  console.info(docs);
+  await collection.drop();
+  console.info('Dropped collection.');
+  const force = false;
+  await client.close(force);
+  console.info('Closed connection to database.');
 }).catch(error => console.error(error));
