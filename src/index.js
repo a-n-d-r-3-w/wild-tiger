20a0b1c468559f0a505ce5db3a8e30a606c2cae6
andr3w <liu.anray+github@gmail.com>
2018-06-22 22:13:13 -0400

    Install and configure Babel CLI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 
 const insertDocuments = (db, callback) => {
   const collection = db.collection('documents');
@@ -12,9 +12,7 @@ const insertDocuments = (db, callback) => {
 };
 
 const findDocuments = (db, callback) => {
-  // Get the documents collection
   const collection = db.collection('documents');
-  // Find some documents
   collection.find({}).toArray((err, docs) => {
     console.log('Found the following records');
     console.log(docs);
@@ -22,7 +20,9 @@ const findDocuments = (db, callback) => {
   });
 };
 
-const uri = 'mongodb://kotetsu:qv4zvvcbfV3XeAxw@cluster0-shard-00-00-spwtd.mongodb.net:27017,cluster0-shard-00-01-spwtd.mongodb.net:27017,cluster0-shard-00-02-spwtd.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+const uri = 'mongodb://kotetsu:qv4zvvcbfV3XeAxw@cluster0-shard-00-00-spwtd.mongodb.net:27017,'
+  + 'cluster0-shard-00-01-spwtd.mongodb.net:27017,cluster0-shard-00-02-spwtd.mongodb.net:27017/test?'
+  + 'ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 MongoClient.connect(uri, (err, client) => {
   if (err) {
     console.error(err);
