a9d3c9a1118fb70b3484b0a6de68f0a04416e011
andr3w <liu.anray+github@gmail.com>
2018-06-24 19:02:16 -0400

    Move credentials into untracked file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
 import 'babel-polyfill';
 import { MongoClient } from 'mongodb';
+import { readFileSync } from 'fs';
 
-const insertDocuments = collection => collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
-const findDocuments = collection => collection.find({}).toArray();
-
-const URI = 'mongodb://kotetsu:qv4zvvcbfV3XeAxw@cluster0-shard-00-00-spwtd.mongodb.net:27017,'
+const mongoDbConfig = JSON.parse(readFileSync('./mongoDb.json', 'utf-8'));
+const URI = `mongodb://${mongoDbConfig.username}:${mongoDbConfig.password}@cluster0-shard-00-00-spwtd.mongodb.net:27017,`
   + 'cluster0-shard-00-01-spwtd.mongodb.net:27017,cluster0-shard-00-02-spwtd.mongodb.net:27017/test?'
   + 'ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+const insertDocuments = collection => collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+const findDocuments = collection => collection.find({}).toArray();
 
 MongoClient.connect(URI).then(async (client) => {
   const db = client.db('wildTigerDb');
