386b6debb714456bd42e75a4bc8d835c53a33634
andr3w <liu.anray+github@gmail.com>
2018-06-26 22:07:01 -0400

    Implement basic authentication

--- src/server.js
+++ src/server.js
@@ -2,4 +2,5 @@ import express from 'express';
 
 const app = express();
 app.use(express.static('public'));
+app.use('/auth0', express.static('node_modules/auth0-js/build'));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
