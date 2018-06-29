c0c0c2ba3b9f2deccdc7827d77520ca7b4d740cc
andr3w <liu.anray+github@gmail.com>
2018-06-28 22:10:55 -0400

    Refactor

---
+++ public/main.js
@@ -0,0 +1,87 @@
+/* eslint-disable no-console,no-alert */
+/* globals window, document, auth0, localStorage, alert */
+window.addEventListener('load', () => {
+  const loginStatus = document.querySelector('.container h4');
+  const homeView = document.getElementById('home-view');
+  const homeViewBtn = document.getElementById('btn-home-view');
+  const logoutBtn = document.getElementById('btn-logout');
+
+  // noinspection ES6ModulesDependencies, NodeModulesDependencies
+  const webAuth = new auth0.WebAuth({
+    domain: 'wild-tiger.auth0.com',
+    clientID: 'xP7Pdp7IfwuHO3hjKPtmlAtFDbO5m23x',
+    responseType: 'token id_token',
+    audience: 'https://wild-tiger.auth0.com/userinfo',
+    scope: 'openid',
+    redirectUri: window.location.href,
+  });
+
+  const loginBtn = document.getElementById('btn-login');
+
+  loginBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    webAuth.authorize();
+  });
+
+  homeViewBtn.addEventListener('click', () => {
+    homeView.style.display = 'inline-block';
+    // loginView.style.display = 'none';
+  });
+
+  function isAuthenticated() {
+    // Check whether the current time is past the
+    // Access Token's expiry time
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+  function displayButtons() {
+    if (isAuthenticated()) {
+      loginBtn.style.display = 'none';
+      logoutBtn.style.display = 'inline-block';
+      loginStatus.innerHTML = 'You are logged in!';
+    } else {
+      loginBtn.style.display = 'inline-block';
+      logoutBtn.style.display = 'none';
+      loginStatus.innerHTML = 'You are not logged in! Please log in to continue.';
+    }
+  }
+
+  function logout() {
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    displayButtons();
+  }
+
+  logoutBtn.addEventListener('click', logout);
+
+  function setSession(authResult) {
+    // Set the time that the Access Token will expire at
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime(),
+    );
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
+
+  function handleAuthentication() {
+    webAuth.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        window.location.hash = '';
+        setSession(authResult);
+        loginBtn.style.display = 'none';
+        homeView.style.display = 'inline-block';
+      } else if (err) {
+        homeView.style.display = 'inline-block';
+        console.log(err);
+        alert(`Error: ${err.error}. Check the console for further details.`);
+      }
+      displayButtons();
+    });
+  }
+
+  handleAuthentication();
+});
--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
-/* eslint-disable no-console */
 import 'babel-polyfill';
-import { MongoClient } from 'mongodb';
-import { readFileSync } from 'fs';
 import './server';
-
-const mongoDbConfig = JSON.parse(readFileSync('./mongoDb.json', 'utf-8'));
-const URI = `mongodb://${mongoDbConfig.username}:${mongoDbConfig.password}@cluster0-shard-00-00-spwtd.mongodb.net:27017,`
-  + 'cluster0-shard-00-01-spwtd.mongodb.net:27017,cluster0-shard-00-02-spwtd.mongodb.net:27017/test?'
-  + 'ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
-const insertDocuments = collection => collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
-const findDocuments = collection => collection.find({}).toArray();
-
-MongoClient.connect(URI).then(async (client) => {
-  const db = client.db('wildTigerDb');
-  console.info('Connected to database.');
-  const collection = db.collection('wildTigerCollection');
-  await insertDocuments(collection);
-  console.info('Inserted documents into the collection.');
-  const docs = await findDocuments(collection);
-  console.info('Found the following records:');
-  console.info(docs);
-  await collection.drop();
-  console.info('Dropped collection.');
-  const force = false;
-  await client.close(force);
-  console.info('Closed connection to database.');
-}).catch(error => console.error(error));
+import './database';
